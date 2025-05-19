0a87270abf1bf982c9cb25ec3cd1fa3bdb4c4317
Lior Singer <liorzinger123@gmail.com>
2025-05-19 18:02:36 +0300

    remove api

--- app/utils/records.ts
+++
@@ -1,8 +0,0 @@
-import { NewTzimer } from './interfaces';
-
-export const NewTzimerTypes: Record<keyof NewTzimer, string> = {
-    name: 'string',
-    description: 'string',
-    location: 'string',
-    region: 'string',
-};
--- prisma/migrations/20250511095226_init/migration.sql
+++ prisma/migrations/20250511095226_init/migration.sql
@@ -1,18 +1,20 @@
 -- CreateTable
-CREATE TABLE `booking` (
+CREATE TABLE `Bookings` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `roomId` INTEGER NOT NULL,
+    `resortId` INTEGER NOT NULL,
     `startDate` DATETIME(3) NOT NULL,
     `endDate` DATETIME(3) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
     INDEX `Booking_roomId_fkey`(`roomId`),
+    INDEX `Bookings_resortId_fkey`(`resortId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `tzimer` (
+CREATE TABLE `Resorts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
@@ -21,12 +23,12 @@ CREATE TABLE `tzimer` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `tzimer_name_key`(`name`),
+    UNIQUE INDEX `Resorts_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `room` (
+CREATE TABLE `Rooms` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `type` VARCHAR(191) NOT NULL,
@@ -36,12 +38,12 @@ CREATE TABLE `room` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `Room_resortId_fkey`(`resortId`),
+    INDEX `Rooms_resortId_fkey`(`resortId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `user` (
+CREATE TABLE `Users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
@@ -50,6 +52,18 @@ CREATE TABLE `user` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `User_resortId_fkey`(`resortId`),
+    INDEX `Users_resortId_fkey`(`resortId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Bookings` ADD CONSTRAINT `Bookings_roomId_fkey` FOREIGN KEY (`roomId`) REFERENCES `Rooms`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Bookings` ADD CONSTRAINT `Bookings_resortId_fkey` FOREIGN KEY (`resortId`) REFERENCES `Resorts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Rooms` ADD CONSTRAINT `Rooms_resortId_fkey` FOREIGN KEY (`resortId`) REFERENCES `Resorts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Users` ADD CONSTRAINT `Users_resortId_fkey` FOREIGN KEY (`resortId`) REFERENCES `Resorts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
